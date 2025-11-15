111f1dcf2612126da22ae2462047fd9be3ca42cb
Mathias Toala <fetoala@outlook.com>
2025-11-15 17:11:23 -0500

    Generate more modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { ProjectsModule } from './projects/projects.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
+// Este es el módulo principal que puede contener a otros módulos
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TasksModule, ProjectsModule, AuthModule, UsersModule] // Conectar el módulo princupal con el resto
 })
 export class AppModule {}
