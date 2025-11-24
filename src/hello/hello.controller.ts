1f5820a6d952b83fee8ee0d5710ea8aecfa6aed6
Mathias Toala <fetoala@outlook.com>
2025-11-23 19:20:56 -0500

    Add Pipes to hello.controller. Create Pipe validateuser to handle http queries of object user

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, HttpCode, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, ParseBoolPipe, ParseIntPipe, Query, Req, Res } from '@nestjs/common';
 import type { Request, Response } from 'express';
+import { ValidateuserPipe } from './pipes/validateuser/validateuser.pipe';
 
 // Controlador generado con comando, se conectó automáticamente al app.module.ts
 @Controller() // Si tiene argumentos, debe existir este nombre en la ruta
@@ -35,5 +36,33 @@ export class HelloController {
     @HttpCode(201)
     somethingNew(){
         return "Algo creado"
+    }
+
+    @Get('ticket/:num')
+    getNumber(@Param('num', ParseIntPipe) num: number){
+        console.log(typeof num)
+        return num + 7;
+        /* Concatena? Se necesita un PIPE!!
+        En este caso el Pipe es ParseIntPipe
+        Recordar: Todo Pipe es una función que recibe un valor, 
+        lo transforma y lo devuelve. 
+        
+        Sirven para: validar datos entrantes, transformar
+        datos entrantes, limpiar datos y rechazar datos inválidos.
+        */
+    }
+
+    @Get('active/:status')
+    isUserActive(@Param('status', ParseBoolPipe) status: Boolean){
+        console.log(typeof status)
+        return status
+    }
+
+    // Crear Pipes propios
+    @Get('/greet')
+    greet(@Query(ValidateuserPipe) query: {name: string, age: number}){
+        console.log(typeof query.name)
+        console.log(typeof query.age)
+        return `Hello ${query.name}, your age is ${query.age}`
     }
 }
